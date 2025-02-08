de1dc9725658442e5baa4d65b551b8cf64634f10
Yonatan Mebratu <ymebratu64@gmail.com>
2025-02-08 21:01:28 +0300

    feat: add Trainee model import to auth-web route for enhanced functionality

--- backend/routes/auth-web.js
+++ backend/routes/auth-web.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
+import Trainee from '../models/Trainee.js';
 
 const router = express.Router();
 
