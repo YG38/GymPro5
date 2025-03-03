aad843acb6cc602aa99fdc8f3a00c2bb76e0b8d1
Yonatan Mebratu <ymebratu64@gmail.com>
2025-03-03 21:50:21 +0300

    jldsf

--- backend/routes/managerRoutes.js
+++ backend/routes/managerRoutes.js
@@ -1,16 +1,21 @@
 import { Router } from 'express';
-import { findByIdAndDelete } from '../models/User';
-import Manager from '../models/Manager';
+import { findByIdAndDelete } from '../models/User.js';
+import Manager from '../models/Manager.js';
 
 const router = Router();
 
 // Delete a user specific to manager's gym
 router.delete('/delete-user/:userId', async (req, res) => {
-    const user = await findByIdAndDelete(req.params.userId);
-    if (user) {
-        res.status(200).json({ message: 'User deleted successfully!' });
-    } else {
-        res.status(404).json({ error: 'User not found' });
+    try {
+        const user = await findByIdAndDelete(req.params.userId);
+        if (user) {
+            res.status(200).json({ message: 'User deleted successfully!' });
+        } else {
+            res.status(404).json({ error: 'User not found' });
+        }
+    } catch (error) {
+        console.error('Error deleting user:', error);
+        res.status(500).json({ error: 'Internal server error' });
     }
 });
 
